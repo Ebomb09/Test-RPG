50606bfe213d861f318745367d5614cf7dc436a9
Ethan Bryson <ebryson09@gmail.com>
2023-01-31 19:24:09 -0800

    void* readability

--- src/interface/mainmenu.cpp
+++ src/interface/mainmenu.cpp
@@ -17,6 +17,6 @@ void mainmenu::draw(){
 	Game->draw_Line(10, 10, (Game->key_Held(SDL_SCANCODE_A)) ? 400 : 200, 300);
 }
 
-void mainmenu::get_string(interface* it, void **args){
+void mainmenu::get_string(interface* it, void* args[]){
 	std::cout << "String entered " << *(std::string*)args[0] << '\n';
 }
