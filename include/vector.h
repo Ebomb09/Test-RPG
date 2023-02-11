4175f25c1171c0f7431434db9437b19fea4105c4
Ethan Bryson <ebryson09@gmail.com>
2023-02-10 22:19:38 -0800

    Implement template for vector and point

--- include/vector.h
+++ include/vector.h
@@ -1,12 +1,42 @@
 #ifndef GAME_VECTOR_H
 #define GAME_VECTOR_H
 
-struct vector2{
-	double x, y;
+template <class T>
+struct tuple_2{
+
+	T x = 0; 
+	T y = 0;
+
+	tuple_2 operator+(const tuple_2<T>& add){
+		return tuple_2{x + add.x, y + add.y};
+	}
+
+	tuple_2& operator+=(const tuple_2<T>& add){
+		x += add.x;
+		y += add.y;
+		return *this;
+	}
+
+	tuple_2 operator-(const tuple_2<T>& sub){
+		return tuple_2{x - sub.x, y - sub.y};
+	}
+
+	tuple_2& operator-=(const tuple_2<T>& sub){
+		x -= sub.x;
+		y -= sub.y;
+		return *this;
+	}
+
+	bool operator==(const tuple_2<T>& comp){
+		return (x == comp.x && y == comp.y);
+	}
+
+	bool operator!=(const tuple_2<T>& comp){
+		return (x != comp.x || y != comp.y);
+	}
 };
 
-struct point{
-	int x, y;
-};
+using vector2 = tuple_2<float>;
+using point = tuple_2<int>;
 
 #endif
