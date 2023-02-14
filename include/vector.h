add8bbf0299f8b51c92e0fe66d011bf70c11441d
Ethan Bryson <ebryson09@gmail.com>
2023-02-13 22:24:47 -0800

    Added * / methods

--- include/vector.h
+++ include/vector.h
@@ -7,36 +7,60 @@ struct tuple_2{
 	T x = 0; 
 	T y = 0;
 
-	tuple_2 operator+(const tuple_2<T>& add){
-		return tuple_2{x + add.x, y + add.y};
+	tuple_2<T> operator+(const tuple_2<T>& add){
+		return tuple_2<T>{x + add.x, y + add.y};
 	}
 
-	tuple_2& operator+=(const tuple_2<T>& add){
+	tuple_2<T>& operator+=(const tuple_2<T>& add){
 		x += add.x;
 		y += add.y;
 		return *this;
 	}
 
-	tuple_2 operator-(const tuple_2<T>& sub){
-		return tuple_2{x - sub.x, y - sub.y};
+	tuple_2<T> operator-(const tuple_2<T>& sub){
+		return tuple_2<T>{x - sub.x, y - sub.y};
 	}
 
-	tuple_2& operator-=(const tuple_2<T>& sub){
+	tuple_2<T>& operator-=(const tuple_2<T>& sub){
 		x -= sub.x;
 		y -= sub.y;
 		return *this;
 	}
 
-	bool operator==(const tuple_2<T>& comp){
+	template <class Tn>
+	tuple_2<T> operator*(const Tn& mult){
+		return tuple_2<T>{x * mult, y * mult};
+	}
+
+	template <class Tn>
+	tuple_2<T>& operator*=(const Tn& mult){
+		x *= mult;
+		y *= mult;
+		return *this;
+	}
+
+	template <class Tn>
+	tuple_2<T> operator/(const Tn& div){
+		return tuple_2<T>{x / div, y / div};
+	}
+
+	template <class Tn>
+	tuple_2<T>& operator/=(const Tn& div){
+		x /= div;
+		y /= div;
+		return *this;
+	}
+
+	bool operator==(const tuple_2& comp){
 		return (x == comp.x && y == comp.y);
 	}
 
-	bool operator!=(const tuple_2<T>& comp){
+	bool operator!=(const tuple_2& comp){
 		return (x != comp.x || y != comp.y);
 	}
 };
 
-using vector2 = tuple_2<float>;
+using vector = tuple_2<float>;
 using point = tuple_2<int>;
 
 #endif
