f4e400e0e4fb1e91b8a65a148ef9770ea1d8296b
Ethan Bryson <ebryson09@gmail.com>
2023-02-13 22:26:39 -0800

    moves lists

--- include/character.h
+++ include/character.h
@@ -1,13 +1,17 @@
 #ifndef GAME_CHARACTERS_H
 #define GAME_CHARACTERS_H
 
+#include <vector>
 #include <string>
 #include "stats.h"
+#include "move.h"
 
 struct character{
 	std::string asset;
 	std::string name;
-	stats stats; 
+	stats stats;
+	std::vector<move*> learned_moves;
+	std::vector<move*> compatible_moves; 
 };
 
 #endif 
